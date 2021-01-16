4e99203c34be3e9a7bb4843fafcf60a493a62686
mleblan67 <mleblan67@charlottesvilleschools.org>
2021-01-15 22:44:55 -0500

    Add functions for buttons and sliders

--- Phone2Pi/MainViewController.swift
+++ Phone2Pi/MainViewController.swift
@@ -14,6 +14,11 @@ class MainViewController: UIViewController {
     var port: String = ""
     //MQTT variables
     var mqttClient: CocoaMQTT!
+    //UI Variables
+    @IBOutlet weak var S1ValText: UILabel!
+    @IBOutlet weak var S2ValText: UILabel!
+    @IBOutlet weak var T1Field: UITextField!
+    
     
     override func viewDidLoad() {
         //connecting to pi with MQTT
@@ -25,13 +30,44 @@ class MainViewController: UIViewController {
         
     }
     
-    @IBAction func buttonDown(_ sender: UIButton) {
-        mqttClient.publish("p2p", withString: "Hello World")
-    }
-    
-    //Unwind segue back to home
+    //Back to home Button
     @IBAction func homeSegueButton(_ sender: UIButton) {
         performSegue(withIdentifier: "homeSegue", sender: nil)
     }
+    //Sliders
+    @IBAction func S1Slider(_ sender: UISlider) {
+        //update text
+        let val: Float = round(sender.value)
+        S1ValText.text = String(val)
+        //send value
+    }
+    @IBAction func S2Slider(_ sender: UISlider) {
+        //update text
+        let val: Float = round(sender.value)
+        S2ValText.text = String(val)
+        //send value
+    }
+    //Buttons
+    @IBAction func B1Button(_ sender: UIButton) {
+        //send value
+    }
+    @IBAction func B2Button(_ sender: UIButton) {
+        //send value
+    }
+    @IBAction func B3Button(_ sender: UIButton) {
+        //send value
+    }
+    @IBAction func B4Button(_ sender: UIButton) {
+        //send value
+    }
+    //Text Field
+    @IBAction func T1SendButton(_ sender: UIButton) {
+        //send value
+        T1Field.text = ""
+    }
+    
+    
+    
+    
     
 }
